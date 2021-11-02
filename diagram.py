b2b8f664faa5c8681dcc29ed95ee535b6ba8a757
ss.lukashin <ss.lukashin@tensor.ru>
2021-11-02 16:48:42 +0700

    fix

--- diagram.py
+++ diagram.py
@@ -5,13 +5,13 @@ from time import sleep
 
 users_path = 'users'
 png_path = 'png'
-test_json = {"1": {"Каким транспортом вы пользуетесь?": {"Автомобиль с ДВС": 0, "Электромобиль": 0, "Элктросамокат": 0, "Велосипед": 0, "Общественный транспорт": 0}},
-             "2": {"Сортируете ли мусор?": {"нет": 0, "да": 0, "иногда": 0}},
-             "3": {"Вы едите мясо/ молочные продукты": {"нет": 0, "да": 0, "я на пути к вегетарианству": 0}},
-             "4": {"Как часто вы совершаете перелёты": {"1 раз в год": 0, "2-3 раза в год": 0, "более 3х раз в год": 0}},
-             "5": {"У вас есть дома пакет с пакетами? Только честно конечно": {"да": 0, "нет": 0, "только бумажный пакет с пакетами": 0, "я давно хожу с эко-пакетом по магазинам": 0}},
-             "6": {"В кофейне вы берете стакан с собой?": {"да": 0, "нет": 0, "я всегда со своей термокружкой": 0}},
-             "7": {"Вы когда-нибудь были в секонд-хенд?": {"да, даже покупал": 0, "нет": 0, "заходил для интереса": 0}},
+test_json = {"1": {"Каким транспортом вы пользуетесь?": {"Автомобиль с ДВС": {"val": 0, "color": "#ff9999"}, "Электромобиль": {"val": 0, "color": "#99ff99"}, "Элктросамокат": {"val": 0, "color": "#ffcc99"}, "Велосипед": {"val": 0, "color": "#66b3ff"}, "Общественный транспорт": {"val": 0, "color": "#999999"}}},
+             "2": {"Сортируете ли мусор?": {"нет": {"val": 0, "color": "#ff9999"}, "да": {"val": 0, "color": "#99ff99"}, "иногда": {"val": 0, "color": "#ffcc99"}}},
+             "3": {"Вы едите мясо/ молочные продукты": {"нет": {"val": 0, "color": "#99ff99"}, "да": {"val": 0, "color": "#ff9999"}, "я на пути к вегетарианству": {"val": 0, "color": "#ffcc99"}}},
+             "4": {"Как часто вы совершаете перелёты": {"1 раз в год": {"val": 0, "color": "#99ff99"}, "2-3 раза в год": {"val": 0, "color": "#ffcc99"}, "более 3х раз в год": {"val": 0, "color": "#ff9999"}}},
+             "5": {"У вас есть дома пакет с пакетами? Только честно конечно": {"да": {"val": 0, "color": "#ff9999"}, "нет": {"val": 0, "color": "#66b3ff"}, "только бумажный пакет с пакетами": {"val": 0, "color": "#ffcc99"}, "я давно хожу с эко-пакетом по магазинам": {"val": 0, "color": "#99ff99"}}},
+             "6": {"В кофейне вы берете стакан с собой?": {"да": {"val": 0, "color": "#ff9999"}, "нет": {"val": 0, "color": "#ffcc99"}, "я всегда со своей термокружкой": {"val": 0, "color": "#99ff99"}}},
+             "7": {"Вы когда-нибудь были в секонд-хенд?": {"да, даже покупал": {"val": 0, "color": "#99ff99"}, "нет": {"val": 0, "color": "#ff9999"}, "заходил для интереса": {"val": 0, "color": "#ffcc99"}}},
              "Счетчик": 0}
 
 
@@ -34,19 +34,20 @@ def main():
         for count, questions in read_file.items():
             for question, answers in questions.items():
                 for answer, value in answers.items():
-                    result_dict[count][question][answer] += value
+                    result_dict[count][question][answer]['val'] += value
     result_dict.pop('Счетчик')
 
     for count, value in result_dict.items():
         file_path = os.path.join(png_path, f"{count}.png")
         question = list(value.keys())[0]
         labels = list(value.get(question).keys())
-        values = list(value.get(question).values())
+        values = [elem.get("val") for elem in list(value.get(question).values())]
+        colors = [elem.get("color") for elem in list(value.get(question).values())]
 
         fig1, ax1 = plt.subplots()
         plt.title(question)
         ax1.pie(values, labels=labels)
-        wedges, texts, autotexts = ax1.pie(values, labels=labels, autopct='%1.2f%%')
+        wedges, texts, autotexts = ax1.pie(values, labels=labels, autopct='%1.2f%%', colors=colors)
         ax1.axis('equal')
         # ax1.legend(loc='upper left', bbox_to_anchor=(1.0, 1.0))
         plt.savefig(file_path)
